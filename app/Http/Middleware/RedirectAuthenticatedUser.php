b967a6d53d2a25899b461bfc74efcbe8a34491e1
alan lemos <amdlemos@gmail.com>
2024-04-17 12:35:35 -0300

    Feat: Add authentication check middleware for /login route

---
+++ app/Http/Middleware/RedirectAuthenticatedUser.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectAuthenticatedUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $guard = null): Response
+    {
+        if (Auth::guard($guard)->check()) {
+            // Usuário está autenticado, redireciona para a rota /home
+            return redirect('/persons');
+        }
+
+        return $next($request);
+    }
+}
